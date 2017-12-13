a06e20a7c6d3698b05a12c13da9379b9c8882c95
Lars <lars@mathem.se>
2017-12-13 14:10:25 +0100

    adding helper for getting current region

---
+++ MhLabs.DataAccessIoC/AWS/Region.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon;
+
+namespace MhLabs.DataAccessIoC.AWS
+{
+    public static class Region
+    {
+        public static RegionEndpoint Current = RegionEndpoint.GetBySystemName(Environment.GetEnvironmentVariable("AWS_DEFAULT_REGION"));
+    }
+}
